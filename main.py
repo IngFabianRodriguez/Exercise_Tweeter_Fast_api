4a87917fb82eaeaf218375571faf4e2fc308ea01
Hector Fabian Rodriguez Acosta <hector.fabian.rodriguez@outlook.com>
2021-10-28 23:09:04 -0500

    Creacion base de aputamientos Users

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Python
+from typing import Optional, List
+
 # Pydantic
 # FastAPI
 from fastapi import FastAPI
@@ -6,13 +8,86 @@ from fastapi import status
 
 
 # Models
-from models import User, UserIn, UserOut
+from models import UserBase, UserLogin, User
 from models import Tweet
 
 app = FastAPI()
 
-
-@app.get(path="/", tags=["Home"], status_code=status.HTTP_200_OK)
+# * Path Operators
+@app.get(path="/", tags=["Home"], status_code=status.HTTP_200_OK, summary="Home")
 def home():
     """Base home, Comprobacion API funcionando"""
     return {"message": "Welcome"}
+
+
+# * Path Operators for User
+@app.post(
+    path="/singup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register User",
+    tags=["Users"],
+)
+def singup():
+    """Register User"""
+    pass
+
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login User",
+    tags=["Users"],
+)
+def login():
+    """Login User"""
+    pass
+
+
+@app.post(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all Users",
+    tags=["Users"],
+)
+def show_all_users():
+    """List users system"""
+    pass
+
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags=["Users"],
+)
+def show_a_user():
+    """Show user in system"""
+    pass
+
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags=["Users"],
+)
+def delete_a_user():
+    """Delete user in system"""
+    pass
+
+
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags=["Users"],
+)
+def update_a_user():
+    """Update user in system"""
+    pass
--- models.py
+++ models.py
@@ -8,20 +8,19 @@ from pydantic import BaseModel
 from pydantic import Field, EmailStr
 
 
-class User(BaseModel):
+class UserBase(BaseModel):
     user_id: UUID = Field(..., alias="id")
     email: EmailStr = Field(..., example="Jhon@doe.com")
-    first_name: str = Field(..., min_length=2, max_length=50, example="John")
-    last_name: str = Field(..., min_length=2, max_length=50, example="Doe")
-    birthday: Optional[date] = Field(default=None)
 
 
-class UserIn(User):
+class UserLogin(UserBase):
     password: str = Field(..., min_length=8, max_length=30)
 
 
-class UserOut(User):
-    pass
+class User(UserBase):
+    first_name: str = Field(..., min_length=2, max_length=50, example="John")
+    last_name: str = Field(..., min_length=2, max_length=50, example="Doe")
+    birthday: Optional[date] = Field(default=None)
 
 
 class Tweet(BaseModel):
@@ -29,4 +28,4 @@ class Tweet(BaseModel):
     content: str = Field(..., min_length=2, max_length=240, example="Hello World")
     created_at: datetime = Field(default=datetime.now())
     updated_at: Optional[datetime] = Field(default=None)
-    by: UserIn = Field(..., alias="User")
+    by: User = Field(..., alias="User")
